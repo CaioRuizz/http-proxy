fc432aa57cd33af8837ae4a2b84e361a13a792ce
Caio Ruiz <cerz@gft.com>
2024-02-04 14:42:50 -0300

    subindo proxy

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Use a imagem oficial do Python como base
+FROM python:3.9
+
+# Configuração do ambiente
+WORKDIR /app
+COPY requirements.txt /app
+
+# Instala as dependências
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copia os arquivos do aplicativo para o contêiner
+COPY server.py /app
+
+# Define a porta em que o aplicativo estará escutando
+EXPOSE 5000
+
+# Comando para executar o aplicativo quando o contêiner iniciar
+CMD ["python", "proxy.py"]
---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, Response
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''}, methods=['GET', 'POST', 'PUT', 'DELETE'])
+@app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def proxy(path):
+    url = f'{path}'
+    headers = {key: value for (key, value) in request.headers if key != 'Host'}
+
+    # Encaminha a solicitação para o endpoint de destino
+    resposta_destino = requests.request(
+        method=request.method,
+        url=url,
+        headers=headers,
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False
+    )
+
+    # Cria uma resposta para o cliente com os dados recebidos do endpoint de destino
+    resposta = Response(response=resposta_destino.content,
+                        status=resposta_destino.status_code,
+                        headers=resposta_destino.headers.items())
+
+    return resposta
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
